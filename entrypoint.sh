33ec51d32bf32c2b56762afd288efd98fe026b9a
kking124 <21278391+kking124@users.noreply.github.com>
2023-02-07 13:37:45 -0500

    add json output option

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,28 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+      "1") 
+      msg="An error is usually an exception that has been caught and not handled."
+      level="ERROR"
       ;;
-      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task."
+      "2")
+      msg="This is less important than debug log and is often used to provide context in the current task."
+      level="INFO"
       ;;
-      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable."
+      "3")
+      msg="A warning that should be ignored is usually at this level and should be actionable."
+      level="WARN"
       ;;
-      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored."
+      "4") 
+      msg="This is a debug log that shows a log that can be ignored."
+      level="DEBUG"
       ;;
    esac
+   if[ -z "${JSON}" ]
+   then
+       echo "$D $level $msg"
+   else
+       echo "{\"time\":\"$D\",\"level\":\"$level\",\"message\":\"$msg\"}"
+   fi
    c=$(( c+1 ))
 done
